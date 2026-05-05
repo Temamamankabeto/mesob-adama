bf602d17146ac09b9cb8ebe0935f7c8df9f16d6d
Temam <temamaman156@gmail.com>
2026-05-05 05:21:12 -0700

    fix hooks barrel imports for users dashboard

--- frontend/hooks/index.ts
+++ frontend/hooks/index.ts
@@ -1,8 +1,4 @@
-export * from "@/hooks/user-management/use-users";
-export { useRolesLiteQuery as useUserRolesLiteQuery } from "@/hooks/user-management/use-users";
-export * from "@/hooks/user-management/use-roles";
-export * from "@/hooks/user-management/use-permissions";
-export * from "@/hooks/shift-management/use-shifts";
-export * from "@/hooks/payment-management/use-payments";
-export * from "@/hooks/report-management/use-reports";
-export * from "@/hooks/notification-management/use-notifications";
+export * from "@/hooks/user/use-users";
+export { useRolesLiteQuery as useUserRolesLiteQuery } from "@/hooks/user/use-users";
+export * from "@/hooks/user/use-roles";
+export * from "@/hooks/user/use-permissions";
