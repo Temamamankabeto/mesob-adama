835ecfa1c5d474cfd439837bd62b2df0c9fc5781
Temam <temamaman156@gmail.com>
2026-05-12 01:22:22 -0700

    Add service form section routes

--- backend/routes/service.php
+++ backend/routes/service.php
@@ -3,122 +3,32 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Api\ServiceController;
-
+use App\Http\Controllers\Api\ServiceFormSectionController;
 use App\Http\Controllers\Api\UserServiceAssignmentController;
 
 Route::middleware('auth:sanctum')->group(function () {
 
-    /*
-    |--------------------------------------------------------------------------
-    | SERVICES
-    |--------------------------------------------------------------------------
-    */
+    Route::get('/services', [ServiceController::class, 'index']);
+    Route::post('/services', [ServiceController::class, 'store']);
+    Route::put('/services/{service}', [ServiceController::class, 'update']);
+    Route::delete('/services/{service}', [ServiceController::class, 'destroy']);
 
-    Route::get(
-        '/services',
-        [ServiceController::class, 'index']
-    );
+    Route::get('/users/{user}/services', [UserServiceAssignmentController::class, 'show']);
+    Route::post('/users/{user}/services', [UserServiceAssignmentController::class, 'assign']);
+    Route::delete('/users/{user}/services/{serviceId}', [UserServiceAssignmentController::class, 'remove']);
+    Route::patch('/users/{user}/services/{serviceId}/toggle', [UserServiceAssignmentController::class, 'toggle']);
+    Route::get('/service-officers', [UserServiceAssignmentController::class, 'officers']);
 
-    Route::post(
-        '/services',
-        [ServiceController::class, 'store']
-    );
+    Route::get('/services/{service}/forms', [ServiceController::class, 'forms']);
+    Route::post('/services/{service}/forms', [ServiceController::class, 'storeForm']);
+    Route::get('/service-forms/{serviceForm}', [ServiceController::class, 'showForm']);
+    Route::put('/service-forms/{serviceForm}', [ServiceController::class, 'updateForm']);
+    Route::delete('/service-forms/{serviceForm}', [ServiceController::class, 'destroyForm']);
+    Route::patch('/service-forms/{serviceForm}/toggle', [ServiceController::class, 'toggleForm']);
 
-    Route::put(
-        '/services/{service}',
-        [ServiceController::class, 'update']
-    );
-
-    Route::delete(
-        '/services/{service}',
-        [ServiceController::class, 'destroy']
-    );
-
-    /*
-    |--------------------------------------------------------------------------
-    | USER SERVICE ASSIGNMENTS
-    |--------------------------------------------------------------------------
-    */
-
-    Route::get(
-        '/users/{user}/services',
-        [UserServiceAssignmentController::class, 'show']
-    );
-
-    Route::post(
-        '/users/{user}/services',
-        [UserServiceAssignmentController::class, 'assign']
-    );
-
-    /*
-    |--------------------------------------------------------------------------
-    | REMOVE ASSIGNMENT
-    |--------------------------------------------------------------------------
-    */
-
-    Route::delete(
-        '/users/{user}/services/{serviceId}',
-        [UserServiceAssignmentController::class, 'remove']
-    );
-
-    /*
-    |--------------------------------------------------------------------------
-    | TOGGLE ASSIGNMENT
-    |--------------------------------------------------------------------------
-    */
-
-    Route::patch(
-        '/users/{user}/services/{serviceId}/toggle',
-        [UserServiceAssignmentController::class, 'toggle']
-    );
-
-    /*
-    |--------------------------------------------------------------------------
-    | LIST OFFICERS WITH SERVICES
-    |--------------------------------------------------------------------------
-    */
-
-    Route::get(
-        '/service-officers',
-        [UserServiceAssignmentController::class, 'officers']
-    );
-
-
-    // service form route   
-
-    /*
-|--------------------------------------------------------------------------
-| SERVICE FORMS
-|--------------------------------------------------------------------------
-*/
-
-Route::get(
-    '/services/{service}/forms',
-    [ServiceController::class, 'forms']
-);
-
-Route::post(
-    '/services/{service}/forms',
-    [ServiceController::class, 'storeForm']
-);
-
-Route::get(
-    '/service-forms/{serviceForm}',
-    [ServiceController::class, 'showForm']
-);
-
-Route::put(
-    '/service-forms/{serviceForm}',
-    [ServiceController::class, 'updateForm']
-);
-
-Route::delete(
-    '/service-forms/{serviceForm}',
-    [ServiceController::class, 'destroyForm']
-);
-
-Route::patch(
-    '/service-forms/{serviceForm}/toggle',
-    [ServiceController::class, 'toggleForm']
-);
+    Route::get('/service-form-sections', [ServiceFormSectionController::class, 'index']);
+    Route::post('/service-form-sections', [ServiceFormSectionController::class, 'store']);
+    Route::get('/service-form-sections/{serviceFormSection}', [ServiceFormSectionController::class, 'show']);
+    Route::put('/service-form-sections/{serviceFormSection}', [ServiceFormSectionController::class, 'update']);
+    Route::delete('/service-form-sections/{serviceFormSection}', [ServiceFormSectionController::class, 'destroy']);
 });
