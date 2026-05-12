5b0b4e457b4c2dae10c5234368bb1f0e3e5cc75c
Temam Aman <temamaman156@gmail.com>
2026-05-12 01:40:11 -0700

    form section

--- backend/routes/service.php
+++ backend/routes/service.php
@@ -29,69 +29,9 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::delete('/service-forms/{serviceForm}', [ServiceController::class, 'destroyForm']);
     Route::patch('/service-forms/{serviceForm}/toggle', [ServiceController::class, 'toggleForm']);
 
-<<<<<<<<< Temporary merge branch 1
-    Route::put(
-        '/services/{service}',
-        [ServiceController::class, 'update']
-    );
-
-    Route::delete(
-        '/services/{service}',
-        [ServiceController::class, 'destroy']
-    );
-
-    /*
-    |--------------------------------------------------------------------------
-    | SERVICE FORMS
-    |--------------------------------------------------------------------------
-    */
-
-    Route::apiResource(
-        'service-forms',
-        ServiceFormController::class
-    );
-
-    /*
-    |--------------------------------------------------------------------------
-    | USER SERVICE ASSIGNMENTS
-    |--------------------------------------------------------------------------
-    */
-
-    Route::get(
-        '/users/{user}/services',
-        [UserServiceAssignmentController::class, 'show']
-    );
-
-    Route::post(
-        '/users/{user}/services',
-        [UserServiceAssignmentController::class, 'assign']
-    );
-
-    Route::delete(
-        '/users/{user}/services/{serviceId}',
-        [UserServiceAssignmentController::class, 'remove']
-    );
-
-    Route::patch(
-        '/users/{user}/services/{serviceId}/toggle',
-        [UserServiceAssignmentController::class, 'toggle']
-    );
-
-    /*
-    |--------------------------------------------------------------------------
-    | OFFICERS
-    |--------------------------------------------------------------------------
-    */
-
-    Route::get(
-        '/service-officers',
-        [UserServiceAssignmentController::class, 'officers']
-    );
-=========
     Route::get('/service-form-sections', [ServiceFormSectionController::class, 'index']);
     Route::post('/service-form-sections', [ServiceFormSectionController::class, 'store']);
     Route::get('/service-form-sections/{serviceFormSection}', [ServiceFormSectionController::class, 'show']);
     Route::put('/service-form-sections/{serviceFormSection}', [ServiceFormSectionController::class, 'update']);
     Route::delete('/service-form-sections/{serviceFormSection}', [ServiceFormSectionController::class, 'destroy']);
->>>>>>>>> Temporary merge branch 2
 });
