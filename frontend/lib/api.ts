a3504914f57c39482cfd953d33694cff379e4317
Temam Aman <temamaman156@gmail.com>
2026-08-26 04:27:14 -0700

    fixed security audit test

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosError, AxiosHeaders, InternalAxiosRequestConfig } from "axios";
 
 const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_URL?.replace(/\/$/, "") ?? "http://127.0.0.1:8000/api";
+  process.env.NEXT_PUBLIC_API_URL?.replace(/\/$/, "") ?? "https://mesobbackend.adamacity.gov.et/api";
 
 let accessToken: string | null = null;
 
